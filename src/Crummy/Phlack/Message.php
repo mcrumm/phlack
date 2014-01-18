0e7be35e25916a4895f5c1608ef08ede4f0610b5
Michael Crumm <mike.crumm@groundctrl.com>
2014-01-18 13:43:31 -0800

    Adding DocBlocks to Message

--- src/Crummy/Phlack/Message.php
+++ src/Crummy/Phlack/Message.php
@@ -6,6 +6,9 @@ class Message
 {
     private $data;
 
+    /**
+     * @param string $text
+     */
     public function __construct($text)
     {
         $this->data = array (
@@ -13,11 +16,18 @@ class Message
         );
     }
 
+    /**
+     * @return string
+     */
     public function getText()
     {
         return $this->data['text'];
     }
 
+    /**
+     * @param $channel
+     * @return $this
+     */
     public function setChannel($channel)
     {
         if (!empty($channel)) {
@@ -26,11 +36,17 @@ class Message
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
     public function getChannel()
     {
         return $this->data['channel'];
     }
 
+    /**
+     * @return string
+     */
     public function __toString()
     {
         return json_encode(array_filter($this->data));
