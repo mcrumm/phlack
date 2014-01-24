8bc76d0a6d9bf3e012ff2d52329783fa4169b2a7
Michael Crumm <mike.crumm@groundctrl.com>
2014-01-24 11:26:08 -0800

    Adding MessageResponse to unify success/error responses

---
+++ spec/Crummy/Phlack/Bridge/Guzzle/Response/MessageResponseSpec.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace spec\Crummy\Phlack\Bridge\Guzzle\Response;
+
+use Guzzle\Http\Message\Response;
+use Guzzle\Service\Command\OperationCommand;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class MessageResponseSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedWith([
+            'status' => '200',
+            'reason' => 'OK',
+            'text'   => ''
+        ]);
+    }
+
+    function it_is_a_response_class()
+    {
+        $this->shouldImplement('\Guzzle\Service\Command\ResponseClassInterface');
+    }
+
+    function it_is_create_from_command(OperationCommand $command)
+    {
+        $command->getResponse()->willReturn(new Response('404', '', ''));
+
+        $this::fromCommand($command)->shouldReturnAnInstanceOf($this);
+    }
+}
---
+++ src/Crummy/Phlack/Bridge/Guzzle/Response/MessageResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Crummy\Phlack\Bridge\Guzzle\Response;
+
+use Guzzle\Common\Collection;
+use Guzzle\Service\Command\OperationCommand;
+use Guzzle\Service\Command\ResponseClassInterface;
+
+class MessageResponse extends Collection implements ResponseClassInterface
+{
+    /**
+     * @see ResponseClassInterface
+     */
+    public static function fromCommand(OperationCommand $command)
+    {
+        $response = $command->getResponse();
+
+        return new self([
+            'status' => $response->getStatusCode(),
+            'reason' => $response->getReasonPhrase(),
+            'text'   => $response->getBody(true)
+        ]);
+    }
+}
