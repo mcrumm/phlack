106f89f6aa8659da3bde726b69bccda2a7dff91f
Michael Crumm <mike.crumm@groundctrl.com>
2014-02-12 00:30:19 -0800

    Adding CommandNameMatcher, extending CommandMatcher, WebHookMatcher from it

--- spec/Crummy/Phlack/Common/Matcher/WebHookMatcherSpec.php
+++ spec/Crummy/Phlack/Common/Matcher/WebHookMatcherSpec.php
@@ -12,7 +12,7 @@ class WebHookMatcherSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType('Crummy\Phlack\Common\Matcher\WebHookMatcher');
-        $this->shouldBeAnInstanceOf('\Crummy\Phlack\Common\Matcher\CommandMatcher');
+        $this->shouldBeAnInstanceOf('\Crummy\Phlack\Common\Matcher\CommandNameMatcher');
     }
 
     function it_matches_webhooks(WebHookInterface $webhook)
---
+++ src/Crummy/Phlack/Common/Matcher/CommandNameMatcher.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Crummy\Phlack\Common\Matcher;
+
+use Crummy\Phlack\WebHook\CommandInterface;
+
+class CommandNameMatcher implements MatcherInterface
+{
+    protected $commandName;
+
+    /**
+     * @param string|null $commandName
+     */
+    public function __construct($commandName = null)
+    {
+        $this->commandName = $commandName;
+    }
+
+    /**
+     * @param CommandInterface $command
+     * @return boolean
+     */
+    public function matches(CommandInterface $command)
+    {
+        return $this->getCommandName() === $command->get('command');
+    }
+
+    /**
+     * @param $command
+     * @return $this
+     */
+    public function setCommandName($command)
+    {
+        $this->commandName = $command;
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getCommandName()
+    {
+        return $this->commandName;
+    }
+}
