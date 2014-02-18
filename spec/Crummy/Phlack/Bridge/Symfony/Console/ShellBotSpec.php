21dec0b41d85b2820a782f0d52f0de41920a46dd
Michael Crumm <mike.crumm@groundctrl.com>
2014-02-17 18:18:16 -0800

    Adding specs for Console classes

---
+++ spec/Crummy/Phlack/Bridge/Symfony/Console/ShellBotSpec.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace spec\Crummy\Phlack\Bridge\Symfony\Console;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ShellBotSpec extends ObjectBehavior
+{
+    function it_is_a_console_application()
+    {
+        $this->shouldHaveType('Crummy\Phlack\Bridge\Symfony\Console\ShellBot');
+        $this->shouldBeAnInstanceOf('\Symfony\Component\Console\Application');
+    }
+
+    function its_definition_calls_for_a_single_command_argument()
+    {
+        /** @var \Symfony\Component\Console\Input\InputDefinition $definition */
+        $definition = $this->getDefinition();
+
+        $definition->hasArgument('command')->shouldReturn(true);
+        $definition->getArguments()->shouldHaveCount(1);
+    }
+}
