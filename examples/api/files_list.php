70f0632a88df4096e6595113e92e935682d19d15
Michael Crumm <mike.crumm@groundctrl.com>
2014-03-04 15:10:05 -0800

    Adding examples for files.list with and without the ResourceIterator

---
+++ examples/api/files_list.php
@@ -0,0 +1,30 @@
+<?php
+
+/** @var \Guzzle\Service\Client $api */
+$api = require __DIR__ . '/api_client.php';
+
+echo 'Fetching Files List...' . PHP_EOL;
+
+$result = $api->listFiles();
+
+if (!$result['ok']) {
+    die('FAIL! Error was: ' . $result['error'] . PHP_EOL);
+}
+
+$paging = $result['paging'];
+while ($paging['pages'] >= $paging['page']) {
+
+    printf(PHP_EOL . 'Page %d of %d (%d total files):' . PHP_EOL,
+        $paging['page'],
+        $paging['pages'],
+        $paging['total']
+    );
+
+    foreach ($result['files'] as $file) {
+        echo "\t" . $file['title'] . PHP_EOL;
+    }
+
+    $nextPage   = ++$paging['page'];
+    $result     = $api->listFiles([ 'page' => $nextPage ]);
+    $paging     = $result['paging'];
+}
