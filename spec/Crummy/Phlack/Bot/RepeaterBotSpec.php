2b95be0ecee48e2fbf800c21279969906fc29bdb
Michael Crumm <mike.crumm@groundctrl.com>
2014-09-11 18:18:41 -0700

    Updating RepeaterBot spec

--- spec/Crummy/Phlack/Bot/RepeaterBotSpec.php
+++ spec/Crummy/Phlack/Bot/RepeaterBotSpec.php
@@ -2,30 +2,43 @@
 
 namespace spec\Crummy\Phlack\Bot;
 
+use Crummy\Phlack\Common\Iterocitor;
 use Crummy\Phlack\Common\Matcher\NonMatcher;
 use Crummy\Phlack\WebHook\CommandInterface;
+use Crummy\Phlack\WebHook\Reply\Reply;
 use Crummy\Phlack\WebHook\WebHook;
 use PhpSpec\ObjectBehavior;
 
 class RepeaterBotSpec extends ObjectBehavior
 {
+    function let(Iterocitor $iterocitor)
+    {
+        $this->beConstructedWith(null, $iterocitor);
+    }
+
     function it_is_a_repeater_bot()
     {
         $this->shouldHaveType('Crummy\Phlack\Bot\RepeaterBot');
         $this->shouldBeAnInstanceOf('\Crummy\Phlack\Bot\AbstractBot');
     }
 
-    function it_does_the_repeater(WebHook $command)
+    function it_does_the_repeater($iterocitor, WebHook $command, Reply $reply)
     {
+        $message    = 'Would you mind stepping down from there, with your license and registration?';
+        $response   = '<@U12345|crumm> Would you mind stepping down from there, with your license and registration?';
+        $reply->offsetGet('text')->willReturn($response);
+        $iterocitor->reply($command, $message)->willReturn($reply);
+
         $command->offsetGet('channel_id')->willReturn('C98765');
         $command->offsetGet('channel_name')->willReturn('group');
         $command->offsetGet('user_id')->willReturn('U12345');
         $command->offsetGet('user_name')->willReturn('crumm');
-        $command->offsetGet('text')->willReturn('Would you mind stepping down from there, with your license and registration?');
+        $command->offsetGet('text')->willReturn($message);
         $command->offsetGet('command')->willReturn('would:');
+
         $this
             ->execute($command)['text']
-                ->shouldReturn('<@U12345|crumm> Would you mind stepping down from there, with your license and registration?');
+                ->shouldReturn($response);
     }
 
     function it_sets_and_gets_a_matcher(NonMatcher $matcher)
@@ -51,8 +64,15 @@ class RepeaterBotSpec extends ObjectBehavior
                 ->during('setMatcher', ['matcher']);
     }
 
-    function it_strips_the_command_from_the_webhook_text(WebHook $command)
+    function it_strips_the_command_from_the_webhook_text($iterocitor, WebHook $command, Reply $reply)
     {
+        $user       = '<@U12345|crumm>';
+        $text       = 'bar';
+        $message    = $user . ' ' . $text;
+
+        $iterocitor->reply($command, $text)->willReturn($reply);
+        $reply->offsetGet('text')->willReturn($message);
+
         $command->offsetGet('channel_id')->willReturn('C98765');
         $command->offsetGet('channel_name')->willReturn('group');
         $command->offsetGet('user_id')->willReturn('U12345');
@@ -61,32 +81,47 @@ class RepeaterBotSpec extends ObjectBehavior
         $command->offsetGet('text')->willReturn('foo: bar');
         $this
             ->execute($command)['text']
-                ->shouldReturn('<@U12345|crumm> bar');
+                ->shouldReturn($message);
     }
 
-    function it_does_not_strip_the_first_word_if_not_the_command(WebHook $command)
+    function it_does_not_strip_the_first_word_if_not_the_command($iterocitor, WebHook $command, Reply $reply)
     {
+        $message    = 'foo bar';
+        $response   = '<@U12345|crumm> foo bar';
+
+        $reply->offsetGet('text')->willReturn($response);
+        $iterocitor->reply($command, $message)->willReturn($reply);
+
         $command->offsetGet('channel_id')->willReturn('C98765');
         $command->offsetGet('channel_name')->willReturn('group');
         $command->offsetGet('user_id')->willReturn('U12345');
         $command->offsetGet('user_name')->willReturn('crumm');
         $command->offsetGet('command')->willReturn('foo:');
-        $command->offsetGet('text')->willReturn('foo bar');
+        $command->offsetGet('text')->willReturn($message);
+
         $this
             ->execute($command)['text']
-            ->shouldReturn('<@U12345|crumm> foo bar');
+            ->shouldReturn($response);
     }
 
-    function it_does_not_strip_multiple_occurrences(WebHook $command)
+    function it_does_not_strip_multiple_occurrences($iterocitor, WebHook $command, Reply $reply)
     {
+        $message    = 'foo: bar foo: bat foo baz foo: you';
+        $repeated   = 'bar foo: bat foo baz foo: you';
+        $response   = '<@U12345|crumm> bar foo: bat foo baz foo: you';
+
+        $reply->offsetGet('text')->willReturn($response);
+        $iterocitor->reply($command, $repeated)->willReturn($reply);
+
         $command->offsetGet('channel_id')->willReturn('C98765');
         $command->offsetGet('channel_name')->willReturn('group');
         $command->offsetGet('user_id')->willReturn('U12345');
         $command->offsetGet('user_name')->willReturn('crumm');
         $command->offsetGet('command')->willReturn('foo:');
-        $command->offsetGet('text')->willReturn('foo: bar foo: bat foo baz foo: you');
+        $command->offsetGet('text')->willReturn($message);
+
         $this
             ->execute($command)['text']
-            ->shouldReturn('<@U12345|crumm> bar foo: bat foo baz foo: you');
+            ->shouldReturn($response);
     }
 }
