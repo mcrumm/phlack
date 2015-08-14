9c3b0439587d51cef2284d2b56957d99da30a895
Michael Crumm <mike@crumm.net>
2015-08-14 14:07:49 -0700

    Simplify message sending

---
+++ examples/custom.php
@@ -0,0 +1,15 @@
+<?php
+
+$client  = require __DIR__ . '/client.php';
+$phlack  = new \Crummy\Phlack\Phlack($client);
+
+$response = $phlack->send([
+    'channel'      => '#random',
+    'icon_emoji'   => ':taco:',
+    'username'     => 'Phlack',
+    'unfurl_links' => true,
+    'text'         => 'I :heart: the <http://api.slack.com|Slack API>!',
+]);
+
+echo 'Response:' . PHP_EOL;
+var_dump($response);
--- examples/simple.php
+++ examples/simple.php
@@ -2,8 +2,8 @@
 
 $client  = require __DIR__ . '/client.php';
 $phlack  = new \Crummy\Phlack\Phlack($client);
-$message = new \Crummy\Phlack\Message\Message('Hello, from phlack!');
 
-echo 'Message Payload:' . PHP_EOL . $message . PHP_EOL;
-echo 'Result:' . PHP_EOL;
-var_dump($phlack->send($message));
+$response = $phlack->send('Hello, from Phlack!');
+
+echo 'Response:' . PHP_EOL;
+var_dump($response);
