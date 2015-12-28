9ed00446179a310bccc0ec7ddb233ae40d92569f
Michael Crumm <mike@crumm.net>
2015-12-28 13:59:45 -0800

    refactor(WebHook): Remove deprecated methods from CommandInterface and WebHookInterface

--- src/Crummy/Phlack/WebHook/WebHookInterface.php
+++ src/Crummy/Phlack/WebHook/WebHookInterface.php
@@ -4,10 +4,4 @@ namespace Crummy\Phlack\WebHook;
 
 interface WebHookInterface extends CommandInterface
 {
-    /**
-     * @return float
-     *
-     * @deprecated Will be removed in 0.6.0
-     */
-    public function getTimestamp();
 }
