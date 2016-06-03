45d7f687ad0e2473e8373611e83f7fda5e141097
Michael Crumm <mike@crumm.net>
2016-06-03 12:28:05 -0700

    docs(MainframeInterface): Add note about callable matchers

--- src/WebHook/MainframeInterface.php
+++ src/WebHook/MainframeInterface.php
@@ -7,6 +7,10 @@ use Crummy\Phlack\WebHook\Matcher\MatcherInterface;
 interface MainframeInterface extends Executable
 {
     /**
+     * Attaches an assistant to the mainframe that can respond to commands.
+     *
+     * If $matcher is provided as a `callable`, it should accept a CommandInterface and return a boolean.
+     *
      * @param Executable                     $assistant Something that can respond to the incoming Command.
      * @param MatcherInterface|callable|null $matcher   Determines whether or not the assistant will respond.
      *
